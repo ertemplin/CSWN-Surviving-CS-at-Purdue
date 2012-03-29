4ecac0d8fdc2d2cfffa506b1748daf5fba48f720
Eric Templin <ertemplin@gmail.com>
2012-03-29 15:11:15 -0300

    Updated algorithm using modulus.

--- rot13.c
+++ rot13.c
@@ -10,11 +10,7 @@
 int main( int argc, char ** argv ) {
 	char c;
 	while( EOF != (c = getchar())) {
-		if( c >= 'a' && c <= 'm' || c >= 'A' && c <= 'M' ) {
-			c = c + 13;
-		} else if ( c >= 'n' && c <= 'z' || c >= 'N' && c <= 'Z' ) {
-			c = c - 13;
-		}
+		c = ((c+13)%26);
 		putchar( c );
 	}
 }
